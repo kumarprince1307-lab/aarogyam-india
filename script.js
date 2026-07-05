87f34cb10dfb17a5538cccc6981dbac6539fc036
kumarprince1307-lab <kumar.prince1307@gmail.com>
2026-07-05 17:08:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Aarogyam India Website Loaded");
+window.onload = function () {
+    console.log("Aarogyam India Website Loaded Successfully");
+
+    const button = document.querySelector(".btn");
+
+    button.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        alert(
+            "🚜 Aarogyam India\n\nWebsite पर काम चल रहा है। जल्द ही किसानों के लिए पूरी डिजिटल सेवा उपलब्ध होगी।"
+        );
+    });
+};
