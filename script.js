f026fe0e4d6a1cfff6f604828077cc50106b2eb6
kumarprince1307-lab <kumar.prince1307@gmail.com>
2026-07-05 19:09:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
 const images = [
- const slides = [
   "banner1.jpeg",
   "banner2.jpeg",
   "banner3.jpeg"
 ];
 
 let index = 0;
-const slider = document.getElementById("slider");
 
-setInterval(() => {
-  index++;
-  if (index >= images.length) {
-    index = 0;
-  }
-  slider.src = images[index];
-}, 3000);
+const slider = document.getElementById("slider");
+const dots = document.querySelectorAll(".dot");
+
+function showSlide(i){
+
+    index = i;
+
+    slider.style.opacity="0";
+
+    setTimeout(()=>{
+
+        slider.src = images[index];
+
+        slider.style.opacity="1";
+
+        dots.forEach(dot=>dot.classList.remove("active"));
+
+        dots[index].classList.add("active");
+
+    },300);
+
+}
+
+dots.forEach((dot,i)=>{
+
+    dot.addEventListener("click",()=>{
+
+        showSlide(i);
+
+    });
+
+});
+
+setInterval(()=>{
+
+    index++;
+
+    if(index>=images.length){
+
+        index=0;
+
+    }
+
+    showSlide(index);
+
+},5000);
